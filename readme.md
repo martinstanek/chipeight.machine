6c25c19d95d3830db8a253e1b1da77cbf7e3db86
Martin Stanek <stanekmartin@bluewin.ch>
2025-12-14 22:45:56 +0100

    fix

--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 ## The Chip8 VM
 
-*... I have done it, they have done it, you have done it, or you'll do it too ...*
+*... I have done it, they have done it, you have done it, and you'll get it done too ...*
 
 ![logo](https://github.com/martinstanek/chipeight.machine/blob/develop/misc/ibm-test.png?raw=true)
+![logo](https://github.com/martinstanek/chipeight.machine/blob/develop/misc/corax-test.png?raw=true)
--- src/ChipEight.Machine/Program.cs
+++ src/ChipEight.Machine/Program.cs
@@ -11,7 +11,8 @@ public static class Program
     {
         Console.WriteLine("Chip8");
 
-        var rom = File.ReadAllBytes("/Users/martinstanek/Documents/dev/git/martinstanek/chipeight.machine/tst/ChipEight.Machine.Tests/Roms/1-chip8-logo.ch8");
-        new Chip().WithRemoteDisplay("http://10.0.1.106:8090").Load(rom) .Run();
+        var rom = File.ReadAllBytes("/Users/martinstanek/Documents/dev/git/martinstanek/chipeight.machine/tst/ChipEight.Machine.Tests/Roms/3-corax+.ch8");
+        //new Chip().WithRemoteDisplay("http://10.0.1.106:8090").Load(rom) .Run();
+        new Chip().Load(rom) .Run();
     }
 }
